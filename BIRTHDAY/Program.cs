fac0e9901dae61a44c2b3c2da45c2cc4dbc24912
Adam Sedlák <Sedlak.Adam@stud.vassboskovice.cz>
2021-10-26 10:27:15 +0200

    Add project files.

---
+++ BIRTHDAY/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BIRTHDAY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kolik ti je let?");
+
+            Console.ReadLine();
+
+            double věk;
+            double narození;
+
+
+
+        }
+    }
+}
